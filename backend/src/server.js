06411bb8411332802ba564b86732eddd88db224c
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2026-01-01 15:36:33 +0100

    change error

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,5 @@
 import app from './app.js';
 
-// ERROR INTENCIONADO: excepción al arrancar el servidor
-throw new Error('Fallo forzado en producción para probar rollback en Render');
-
 // process.env.PORT es la variable que usará Render.
 // Si no existe (en local), usará el puerto 3000 por defecto.
 const PORT = process.env.PORT || 3000;
